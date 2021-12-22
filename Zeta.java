d86a9d184e40cff5e93ff2ca57c9a31e36adf6ff
monikaPatwari <96533513+monikaPatwari@users.noreply.github.com>
2021-12-22 19:32:40 +0530

    Add files via upload

---
+++ Zeta.java
@@ -0,0 +1,16 @@
+package coding;
+
+public class Zeta {
+
+    public static void main (String args[]){
+
+        square sq = new square((float) 3.4);
+        sq.area();
+        Circle cr = new Circle(2);
+        cr.area();
+
+
+    }
+
+
+}
